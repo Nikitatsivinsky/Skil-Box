ad1b13411ff4e48ffd68705d9b1768b895b2d3f2
AngelOfDeath <logbookserv@gmail.com>
2022-03-25 17:17:39 +0200

    04.student bug fix

--- ProjectSkillBox/Homework/lesson_003/04_student.py
+++ ProjectSkillBox/Homework/lesson_003/04_student.py
@@ -14,13 +14,14 @@ plus_proc = 0
 expenses_2 = expenses
 month = 1
 total_cash_2 = 0
-while month < 11:
+first_month = expenses_2 - educational_grant
+print('За ', month, ' месяц, - ', first_month, ' процентов.')
+while month < 10:
     month += 1 #плюс месяц
     plus_proc = int((expenses_2/100)*3)
     expenses_2 += int(plus_proc)
     total_cash_2 += int(expenses_2-10000)
-    print('За ', month-1, ' месяц, - ', expenses_2-10000, ' процентов.')
+    print('За ', month, ' месяц, - ', expenses_2-10000, ' процентов.')
 
-
-print('Студенту надо попросить ', total_cash_2,' рублей')
+print('Студенту надо попросить ', total_cash_2+first_month,' рублей')
 
