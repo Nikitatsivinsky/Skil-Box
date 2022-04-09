39618c6aad501217871d64910426ec1372ffa19e
AngelOfDeath <logbookserv@gmail.com>
2022-04-09 22:38:36 +0300

    02_district.py Done

--- ProjectSkillBox/Homework/lesson_005/01_inhabitants.py
+++ ProjectSkillBox/Homework/lesson_005/01_inhabitants.py
@@ -2,13 +2,14 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
-import room_1,room_2
+import room_1
+import room_2
+
 
 def residents(folks, house):
     for human in folks:
-        print('В комнате ', house.__name__ , ' живет: ', human)
+        print('В комнате ', house.__name__, ' живет: ', human)
 
 
-residents(folks=room_1.folks, house= room_1)
-residents(folks=room_2.folks, house= room_2)
-
+residents(folks=room_1.folks, house=room_1)
+residents(folks=room_2.folks, house=room_2)
--- ProjectSkillBox/Homework/lesson_005/02_district.py
+++ ProjectSkillBox/Homework/lesson_005/02_district.py
@@ -5,8 +5,20 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
 
+from district.central_street.house1.room1 import folks as cs_h1_r1
+from district.central_street.house1.room2 import folks as cs_h1_r2
+from district.central_street.house2.room1 import folks as cs_h2_r1
+from district.central_street.house2.room2 import folks as cs_h2_r2
+from district.soviet_street.house1.room1 import folks as ss_h1_r1
+from district.soviet_street.house1.room2 import folks as ss_h1_r2
+from district.soviet_street.house2.room1 import folks as ss_h2_r1
+from district.soviet_street.house2.room2 import folks as ss_h2_r2
 
+folks = (cs_h1_r1, cs_h1_r2, cs_h2_r1, cs_h2_r2, ss_h1_r1, ss_h1_r2, ss_h2_r1, ss_h2_r2)
+folks_list = []
 
+for join in folks:
+    folks_list.extend(join)
 
+print(f'На районе живут :', (' , '.join(folks_list)))
