1cd81a963e43d9ccf1b1bbd7bf45c20f72c2796a
AngelOfDeath <logbookserv@gmail.com>
2022-05-09 19:41:12 +0300

    02_alchemy.py  DONE

--- ProjectSkillBox/Homework/lesson_007/02_alchemy.py
+++ ProjectSkillBox/Homework/lesson_007/02_alchemy.py
@@ -19,7 +19,151 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water():
+
+    def __init__(self):
+        self.name = 'Вода'
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Ground):
+            return Dirt()
+        else:
+            return None
+
+class Air():
+
+    def __init__(self):
+        self.name = 'Воздух'
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Dust()
+        else:
+            return None
+
+class Fire():
+
+    def __init__(self):
+        self.name = 'Огонь'
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Lava()
+        else:
+            return None
+
+class Ground():
+
+    def __init__(self):
+        self.name = 'Земля'
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+        else:
+            return None
+
+
+
+class Storm():
+
+    def __init__(self):
+        self.name = 'Шторм'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Steam():
+
+    def __init__(self):
+        self.name = 'Пар'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dirt():
+
+    def __init__(self):
+        self.name = 'Грязь'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Lightning():
+
+    def __init__(self):
+        self.name = 'Молния'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dust():
+
+    def __init__(self):
+        self.name = 'Пыль'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Lava():
+
+    def __init__(self):
+        self.name = 'Лава'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Ground(), '=', Water() + Ground())
+
+print(Fire(), '+', Air(), '=', Fire() + Air())
+print(Fire(), '+', Water(), '=', Fire() + Water())
+print(Fire(), '+', Ground(), '=', Fire() + Ground())
+
+print(Air(), '+', Water(), '=', Air() + Water())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Ground(), '=', Air() + Ground())
+
+print(Ground(), '+', Water(), '=', Ground() + Water())
+print(Ground(), '+', Air(), '=', Ground() + Air())
+print(Ground(), '+', Fire(), '=', Ground() + Fire())
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
