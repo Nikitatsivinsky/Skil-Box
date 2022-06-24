e13096606c1d7728aedc99bec8e5a4f04c102272
AngelOfDeath <logbookserv@gmail.com>
2022-06-24 21:14:01 +0300

    log_parser2.py 1st part Done

--- ProjectSkillBox/Homework/lesson_009/log parser2.py
+++ ProjectSkillBox/Homework/lesson_009/log parser2.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from datetime import timedelta
 
 
 class LogFile:
-
     __name = None
+    log_entries = []
+    log_entries_grp = {}
 
     def __init__(self, file_name):
         self.set_name(file_name)
@@ -15,6 +15,11 @@ class LogFile:
     def get_name(self):
         return self.__name
 
+    def write_to_file(self, file):
+        for lg in self.log_entries_grp.items():
+            time_value = lg[0].strftime("%Y-%m-%d %H:%M")
+            file.write(f'[{time_value}] {lg[1]}' + '\n')
+
 
 class LogEntry:
     date = None
@@ -23,8 +28,7 @@ class LogEntry:
     def __init__(self, line):
         time_text = str(line[1:27])
         self.date = datetime.strptime(time_text, "%Y-%m-%d %H:%M:%S.%f")
-        self.date.resolution = timedelta(seconds=1)
-        self.value = str(line[29:]).strip()                        
+        self.value = str(line[29:]).strip()
 
     def to_txt(self):
         return f'{self.date} {self.value}'
@@ -38,8 +42,6 @@ class LogEntryNOK(LogEntry):
 
 class LogNOK(LogFile):
 
-    log_entries = []
-
     def __init__(self, file_name):
         super().__init__(file_name)
         self.read_from_file()
@@ -49,21 +51,22 @@ class LogNOK(LogFile):
             for line in file:
                 self.log_entries.append(LogEntryNOK(line))
 
-class LogNOKGroupByMinute(LogNOK):
 
-    log_entries_grp = []
+class LogNOKGroupByMinute(LogNOK):
 
     def __init__(self, file_name):
         super().__init__(file_name)
+        self.group()
 
     def group(self):
         for e in self.log_entries:
             if e.is_not_ok():
-                e_round = e
-                e_round.date.microsecond = 0  # datetime(year, month, day[, hour[, minute[, second[, microsecond[,tzinfo]
-                e_round.date.second = 0
-                if not e_round in self.log_entries_grp:
-                    self.log_entries_grp.append(e_round)
+                date_round = e.date
+                date_round = date_round.replace(microsecond=0, second=0) # datetime(year, month, day[, hour[, minute[, second[, microsecond[,tzinfo]
+                if not (date_round in self.log_entries_grp):
+                    self.log_entries_grp[date_round] = 1
+                else:
+                    self.log_entries_grp[date_round] += 1
 
 
 
@@ -71,11 +74,8 @@ class LogNOKGroupByMinute(LogNOK):
 
 
 
+x = LogNOKGroupByMinute('events.txt')
 
-
-x = LogNOK('events.txt')
-
-
-
-
-
+with open("result.txt", mode='w+', encoding='utf8') as file:
+    file.write(f"{'*' * 10} Start Analyzing file {'events.txt'}!{'*' * 10}\n\n")
+    x.write_to_file(file)
