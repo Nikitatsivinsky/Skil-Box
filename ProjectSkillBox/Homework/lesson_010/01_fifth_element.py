f3cc37b1d219a127f8e676b33802a78d44e63a3c
niki <Nikitatsivinsky@gmail.com>
2022-07-14 17:15:16 +0300

    01_fifth_element.py Done

--- ProjectSkillBox/Homework/lesson_010/01_fifth_element.py
+++ ProjectSkillBox/Homework/lesson_010/01_fifth_element.py
@@ -2,12 +2,35 @@
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
-BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+def input_value():
+    return input('Если хочешь что-нибудь сделать, сделай это сам: ')
+
+def try_leeloo():
+    global leeloo, input_data
+    try:
+        leeloo = int(input_data[4])
+    except ValueError as exc:
+        print(f'Невозможно преобразовать к числу введенные данные {input_data[4]}. Ошибка:{exc}')
+        input_data = input_value()
+        try_leeloo()
+    except IndexError as exc:
+        print(f'Выход за границы списка в введенных данных: {exc}')
+        input_data = input_value()
+        try_leeloo()
+    except Exception as exc:
+        print(f'Ошибка преобразования в строку: {exc}')
+        input_data = input_value()
+        try_leeloo()
+    else:
+        result = BRUCE_WILLIS * leeloo
+        print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+BRUCE_WILLIS = 42
+leeloo = None
+
+input_data = input_value()
+try_leeloo()
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
