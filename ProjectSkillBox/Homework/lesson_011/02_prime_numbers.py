00ca9bf0ea5c0b0054ae0d7502c524123913842a
niki <Nikitatsivinsky@gmail.com>
2022-07-20 22:17:32 +0300

    02_prime_numbers.py Done

--- ProjectSkillBox/Homework/lesson_011/02_prime_numbers.py
+++ ProjectSkillBox/Homework/lesson_011/02_prime_numbers.py
@@ -4,15 +4,18 @@
 # Есть функция генерации списка простых чисел
 
 
-def get_prime_numbers(n):
-    prime_numbers = []
-    for number in range(2, n+1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
-            prime_numbers.append(number)
-    return prime_numbers
+# def get_prime_numbers(n):
+#     prime_numbers = []
+#     for number in range(2, n+1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#     return prime_numbers
+#
+# for i in get_prime_numbers(10000):
+#     print(i)
 
 # Часть 1
 # На основе алгоритма get_prime_numbers создать класс итерируемых обьектов,
@@ -21,14 +24,39 @@ def get_prime_numbers(n):
 # Распечатать все простые числа до 10000 в столбик
 
 
-class PrimeNumbers:
-    pass
-    # TODO здесь ваш код
-
-
-prime_number_iterator = PrimeNumbers(n=10000)
-for number in prime_number_iterator:
-    print(number)
+# class PrimeNumbers:
+#
+#     def __init__(self, count):
+#
+#         self.count = count
+#         self.prime_numbers = self.get_prime_numbers()
+#
+#     def __iter__(self):
+#         self.i = -1
+#         return self
+#
+#     def __next__(self):
+#         try:
+#             self.i += 1
+#             return self.prime_numbers[self.i]
+#         except IndexError:
+#             raise StopIteration
+#
+#     def get_prime_numbers(self):
+#         prime_numbers = []
+#         for number in range(2, self.count+1):
+#             for prime in prime_numbers:
+#                 if number % prime == 0:
+#                     break
+#             else:
+#                 prime_numbers.append(number)
+#         return prime_numbers
+#
+#
+#
+# prime_number_iterator = PrimeNumbers(count=10000)
+# for number in prime_number_iterator:
+#     print(number)
 
 
 # TODO после подтверждения части 1 преподователем, можно делать
@@ -37,14 +65,19 @@ for number in prime_number_iterator:
 # Распечатать все простые числа до 10000 в столбик
 
 
-def prime_numbers_generator(n):
-    pass
-    # TODO здесь ваш код
-
-
-for number in prime_numbers_generator(n=10000):
-    print(number)
-
+# def get_prime_numbers(n):
+#     prime_numbers = []
+#     for number in range(2, n+1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#             yield number
+#     return
+#
+# for number in get_prime_numbers(n=10000):
+#     print(number)
 
 # Часть 3
 # Написать несколько функций-фильтров, которые выдает True, если число:
@@ -61,3 +94,95 @@ for number in prime_numbers_generator(n=10000):
 # простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
 #
 # Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+
+def get_prime_numbers(n):
+    prime_numbers = []
+    for number in range(2, n+1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+
+            yield number, is_lucky(number), is_palindromic(number)
+    return
+
+def is_lucky(number):
+
+    list_number = []
+    first_part = 0
+    second_part = 0
+    counter = 0
+
+    for number in str(number):
+        list_number.append(number)
+
+    if len(list_number) == 1:
+        return False
+
+    if len(list_number) % 2 == 0:
+        for i in range(0, len(list_number)):
+            if counter == len(list_number) / 2:
+                first_part += int(list_number[i])
+            else:
+                counter += 1
+                second_part += int(list_number[i])
+        if first_part == second_part:
+            return True
+        else:
+            return False
+    else:
+        for i in range(0, len(list_number)):
+            if not counter > round(len(list_number) / 2) - 1:
+                counter += 1
+                first_part += int(list_number[i])
+            elif counter == round((len(list_number) / 2)):
+                counter += 1
+                continue
+            else:
+                counter += 1
+                second_part += int(list_number[i])
+        if first_part == second_part:
+            return True
+        else:
+            return False
+
+def is_palindromic(number):
+
+    list_number = []
+
+    for number in str(number):
+        list_number.append(number)
+
+    if len(list_number) == 1:
+        return False
+
+    for i in range(0, round((len(list_number) / 2))):
+        if len(list_number) % 2 == 0:
+            list_number_reverse = list_number[::-1]
+            if list_number == list_number_reverse:
+                return True
+            else:
+                return False
+        else:
+            list_number_reverse = list_number[::-1]
+            center_number = list_number_reverse[round((len(list_number) / 2))]
+            list_number.remove(center_number)
+            list_number_reverse.remove(center_number)
+            if list_number == list_number_reverse:
+                return True
+            else:
+
+                return False
+
+
+
+for number, is_lucky_num, is_palindromic_num in get_prime_numbers(n=10000):
+    if is_lucky_num and is_palindromic_num:
+        print(f'Число {number} счастливое и палиндромное')
+    elif is_lucky_num:
+        print(f'Число {number} счастливое')
+    elif is_palindromic_num:
+        print(f'Число {number} палиндромное')
+    else:
+        print(f'Число {number} обычное')
