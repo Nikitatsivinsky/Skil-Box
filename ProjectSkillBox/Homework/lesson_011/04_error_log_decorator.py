b35c4e0544cacd2fc40b47d99f9ee239baf9ee2f
niki <Nikitatsivinsky@gmail.com>
2022-07-22 00:50:58 +0300

    04_error_log_decorator.py Done

--- ProjectSkillBox/Homework/lesson_011/04_error_log_decorator.py
+++ ProjectSkillBox/Homework/lesson_011/04_error_log_decorator.py
@@ -8,18 +8,31 @@
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+def log_errors(file_name):
+    def decoration_func(func):
+        def catching_exceptions(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except Exception as exc:
+                line_exc = f'<{func.__name__}> <{args}> <{type(exc).__name__}> <{exc}>'
+                print(f'Записываю в {file_name} строку: {line_exc}')
+                with open(file_name, mode='a') as log_file:
+                    log_file.write(line_exc + '\n')
+                func(*args, **kwargs)
+
+        return catching_exceptions
+
+    return decoration_func
 
 
 # Проверить работу на следующих функциях
-@log_errors
+
+@log_errors(file_name='function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors(file_name='function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -38,13 +51,16 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
+
 for line in lines:
     try:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
-
+try:
+    perky(param=42)
+except Exception as exc:
+    print(f'Invalid format: {exc}')
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
@@ -52,4 +68,3 @@ perky(param=42)
 # @log_errors('function_errors.log')
 # def func():
 #     pass
-
