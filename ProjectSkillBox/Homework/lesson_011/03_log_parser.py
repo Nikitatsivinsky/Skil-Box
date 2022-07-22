ac92d91c1bd7f055ead71d59b6780dda858c4351
niki <Nikitatsivinsky@gmail.com>
2022-07-22 17:51:51 +0300

    03_log_parser.py (2 variation)

--- ProjectSkillBox/Homework/lesson_011/03_log_parser.py
+++ ProjectSkillBox/Homework/lesson_011/03_log_parser.py
@@ -8,7 +8,6 @@
 
 # _____________________________________________________Итератор_________________________________________________________
 
-
 # from datetime import datetime
 #
 #
@@ -63,52 +62,52 @@
 # for group_time, event_count in grouped_events:
 #     print(f'[{group_time}] {event_count}')
 
-
 # ______________________________________________________Генератор_______________________________________________________
 
-from datetime import datetime
-
-name_input_file = 'events.txt'
-
-
-def get_line(name_parsing_file):
-    def decorator_func(func):
-        log_entries_dict = {}
-        try:
-            with open(name_parsing_file, mode='r', ) as file:
-                for line_in_file in file:
-                    try:
-                        time_text = str(line_in_file[1:27])
-                        date = datetime.strptime(time_text, "%Y-%m-%d %H:%M:%S.%f")
-                        date = date.strftime("%Y-%m-%d %H:%M")
-                        value = str(line_in_file[29:]).strip()
-                        func(value, date, log_entries_dict)
-                    except ValueError as exc:
-                        print(f'Ошибка в файле {name_input_file}, в строке: {line_in_file}'
-                              f'Ошибка {type(exc).__name__}: {exc}\n'
-                              f'Пропускаю строчку.\n')
-                        pass
-
-        finally:
-            for return_line, value in log_entries_dict.items():
-                yield f'[{return_line}] {value}'
-
-    return decorator_func
-
-
-@get_line(name_input_file)
-def if_nok_in_line(value, date, log_entries_dict):
-    if value == 'NOK':
-        if date in log_entries_dict:
-            log_entries_dict[date] += 1
-        else:
-            log_entries_dict[date] = 1
-
-
-for line in if_nok_in_line:
-    print(line)
-
+# from datetime import datetime
 #
+# name_input_file = 'events.txt'
+#
+#
+# def get_line(name_parsing_file):
+#     def decorator_func(func):
+#         log_entries_dict = {}
+#         try:
+#             with open(name_parsing_file, mode='r', ) as file:
+#                 for line_in_file in file:
+#                     try:
+#                         time_text = str(line_in_file[1:27])
+#                         date = datetime.strptime(time_text, "%Y-%m-%d %H:%M:%S.%f")
+#                         date = date.strftime("%Y-%m-%d %H:%M")
+#                         value = str(line_in_file[29:]).strip()
+#                         func(value, date, log_entries_dict)
+#                     except ValueError as exc:
+#                         print(f'Ошибка в файле {name_input_file}, в строке: {line_in_file}'
+#                               f'Ошибка {type(exc).__name__}: {exc}\n'
+#                               f'Пропускаю строчку.\n')
+#                         pass
+#
+#         finally:
+#             for return_line, value in log_entries_dict.items():
+#                 yield f'[{return_line}] {value}'
+#
+#     return decorator_func
+#
+#
+# @get_line(name_input_file)
+# def if_nok_in_line(value, date, log_entries_dict):
+#     if value == 'NOK':
+#         if date in log_entries_dict:
+#             log_entries_dict[date] += 1
+#         else:
+#             log_entries_dict[date] = 1
+#
+#
+# for line in if_nok_in_line:
+#     print(line)
+
+# _____________________________________________________________________________________________________________________
+
 # на консоли должно появится что-то вроде
 #
 # [2018-05-17 01:57] 1234
