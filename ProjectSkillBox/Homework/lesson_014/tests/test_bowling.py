c72f560e2549c8b6c918f87d7e8e2b1ac075c415
niki <Nikitatsivinsky@gmail.com>
2022-09-03 14:57:34 +0300

    Tests for bowling.py Done

---
+++ ProjectSkillBox/Homework/lesson_014/tests/test_bowling.py
@@ -0,0 +1,25 @@
+import unittest
+from bowling import BowlingScoreEngine
+
+class BowlingScoreEngineTest(unittest.TestCase):
+
+
+    def test_bowling(self):
+        self.assertEqual(BowlingScoreEngine('XXXXXXXXXX').score, 200)
+        self.assertEqual(BowlingScoreEngine('9-9-9-9-9-').score, 45)
+        self.assertEqual(BowlingScoreEngine('5/5/5/5/5/').score, 75)
+        self.assertEqual(BowlingScoreEngine('Х4/34').score, 42)
+        self.assertEqual(BowlingScoreEngine('X7/9-X-88/').score, 88)
+        self.assertEqual(BowlingScoreEngine('').score, 0)
+
+    def test_bowling_error(self):
+        self.assertRaises(ValueError, BowlingScoreEngine, 'XXXXXXXXXXX')
+        self.assertRaises(ValueError, BowlingScoreEngine, 'x')
+        self.assertRaises(ValueError, BowlingScoreEngine, '!')
+        self.assertRaises(ValueError, BowlingScoreEngine, '0')
+        self.assertRaises(ValueError, BowlingScoreEngine, 'Х4|34')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
